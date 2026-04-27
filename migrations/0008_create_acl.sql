3991dc8c7de2ddd8b4a27b9e7601f0c6eedf083e
Craig Jarvis <github.uplifting655@passmail.net>
2026-04-28 07:43:50 +1000

    db: document why acl.owner_account_id is intentionally unindexed
    D5 from the hardening TODO.

--- migrations/0008_create_acl.sql
+++ migrations/0008_create_acl.sql
@@ -1,3 +1,4 @@
+-- owner_account_id is intentionally unindexed: "list all ACLs owned by X" is not a hot query path
 CREATE TABLE acl (
     id                UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
     name              TEXT        NOT NULL,
